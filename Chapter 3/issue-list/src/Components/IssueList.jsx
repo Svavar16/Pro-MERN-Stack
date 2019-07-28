0afceb5460ff1ab6355288fcc5018edd0a3f1ba7
svavar16 <spg009@gmail.com>
2019-07-28 22:36:38 +0000

    adding BorderWrap

--- Chapter 3/issue-list/src/Components/IssueList.jsx
+++ Chapter 3/issue-list/src/Components/IssueList.jsx
@@ -1,10 +1,79 @@
 import React, { Component } from 'react'
 
+class BorderWrap extends Component {
+    render() {
+        const borderedStyle = {border: '1px solid silver', padding: 6}
+        return (
+            <div style={borderedStyle}>{this.props.children}</div>
+        )
+    }
+}
+
+class IssueRow extends Component {
+    render() {
+        const boderStyle = {border: '1px solid silver', padding: 4};
+
+        return (
+            <tr>
+                
+                <td style={boderStyle}>{this.props.issue_id} </td>
+                <BorderWrap>
+                    <td style={boderStyle}>{this.props.children} </td>
+                </BorderWrap>
+            </tr>
+        )
+    }
+}
+
+class IssueFilter extends Component {
+    render(){
+        return(
+            <div>
+                this is a  placeholder for a table of issues.
+            </div>
+        )
+    }
+}
+
+class IssueTable extends Component {
+    render(){
+        const borderedStyle = {border: '1px solid silver', padding: 6}
+        return (
+            <table style={{borderCollapse: 'collapse'}}>
+                <thead>
+                    <th style={borderedStyle}>Id</th>
+                    <th style={borderedStyle}>Title</th>
+                </thead>
+                <tbody>
+                    <IssueRow issue_id={1}>Error in the console when clicking add</IssueRow>
+                    
+                        <IssueRow issue_id={2}>Missing bottom border on panes</IssueRow>
+                </tbody>
+            </table>
+        )
+    }
+}
+
+class IssueAdd extends Component {
+    render() {
+        return(
+            <div>
+                this is a placeholder for IssueAdd.
+            </div>
+        )
+    }
+}
+
 export default class IssueList extends Component {
     render() {
         return (
             <div>
-                this is a placeholder for IssueList.
+                <h1>this is a placeholder for IssueList.</h1>
+                <IssueFilter />
+                <hr/>
+                <IssueTable />
+                <hr/>
+                <IssueAdd />
             </div>
         )
     }
