561ed9a2fcf87d70775e46dfc8a711d570339bb2
svavar16 <spg009@gmail.com>
2019-07-28 14:16:59 +0000

    adding react to chapter 3 and 4

---
+++ Chapter 3/issue-list/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import IssueList from './Components/IssueList'
+
+function App() {
+  return (
+    <IssueList />
+  );
+}
+
+export default App;
---
+++ Chapter 3/issue-list/src/Components/IssueList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class IssueList extends Component {
+    render() {
+        return (
+            <div>
+                this is a placeholder for IssueList.
+            </div>
+        )
+    }
+}
